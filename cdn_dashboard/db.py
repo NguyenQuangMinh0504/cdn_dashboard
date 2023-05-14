2372343791e409e8244aa6af7681aa389ac8a0e9
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-05-14 14:35:24 +0700

    .

--- cdn_dashboard/db.py
+++ cdn_dashboard/db.py
@@ -2,6 +2,6 @@ import pymongo
 import redis
 
 mongo_client = pymongo.MongoClient(host="127.0.0.1")
-domain_table_rdb = redis.Redis(host="127.0.0.1", db=0)
+domain_table_rdb = redis.Redis(host="127.0.0.1", db=0, port=6380)
 domain_table = mongo_client["user"]["domain"]
 
