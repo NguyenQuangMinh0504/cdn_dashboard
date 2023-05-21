887b405909a955f3cdf36e469894b5a9c2c97c35
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-05-21 16:43:34 +0700

    Update utils.py

--- cdn_dashboard/utils.py
+++ cdn_dashboard/utils.py
@@ -6,10 +6,9 @@ def get_domain_slug(url):
     Trả về slug của domain dựa trên input url
     """
     parse_url = urlparse(url=url)
+    print("Net loc is", parse_url.netloc)
     netloc_parts = parse_url.netloc.split(".")
-
-    print("net loc part is", netloc_parts)
-
+    
     if len(netloc_parts) > 2:
         return netloc_parts[1]
     return netloc_parts[0]
