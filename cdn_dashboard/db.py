05c91f19391efc77337f5df82eccdc314a751542
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-09-28 14:43:21 +0700

    redis using port 6379 instead of 6380

--- cdn_dashboard/db.py
+++ cdn_dashboard/db.py
@@ -2,8 +2,8 @@ import pymongo
 from redis import Redis
 
 mongo_client = pymongo.MongoClient(host="127.0.0.1")
-domain_table_rdb = Redis(host="127.0.0.1", db=0, port=6380)
-cache_key_setting_rdb = Redis(host="127.0.0.1", db=1, port=6380)
-total_bytes_sent_rdb = Redis(host="127.0.0.1", db=2, port=6380)
+domain_table_rdb = Redis(host="127.0.0.1", db=0, port=6379)
+cache_key_setting_rdb = Redis(host="127.0.0.1", db=1, port=6379)
+total_bytes_sent_rdb = Redis(host="127.0.0.1", db=2, port=6379)
 domain_table = mongo_client["user"]["domain"]
 user_table = mongo_client["user"]["user"]
