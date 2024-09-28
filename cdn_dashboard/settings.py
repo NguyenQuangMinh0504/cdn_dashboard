867c51f8e2e05521fd9aac0b21284f003813a35e
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2024-09-28 14:58:08 +0700

    add static_root folder

--- cdn_dashboard/settings.py
+++ cdn_dashboard/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = "/var/www/sapphirecdn.com/static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
